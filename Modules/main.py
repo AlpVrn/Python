68c9517bf59b9e4dafe4166715c3771f13866717
AlpVrn <mhmtalpvrn@gmail.com>
2022-11-22 15:38:28 +0300

    Duzeltildi

---
+++ Modules/main.py
@@ -0,0 +1,12 @@
+import mod
+
+# result = help(mod)
+# result = help(mod.func)
+result = mod.number
+result = mod.person["name"]
+result = mod.person["age"]
+result = mod.func(10)
+
+p = mod.Person()
+p.speak()
+print(result)
---
+++ Modules/mod.py
@@ -0,0 +1,21 @@
+'''Modul Hakkında bilgilendirme'''
+
+print('Modul eklendi')
+
+number = 10
+number = [1,2,3]
+
+person ={
+    "name" : "Ali",
+    "age" : "25",
+    "city" : "istanbul"
+}
+
+def func(x):
+    '''fonksiyon için değerlendirme'''
+    print(f'x : {x}')
+
+
+class Person:
+    def speak(self):
+        print('AYEM SPEAK')
