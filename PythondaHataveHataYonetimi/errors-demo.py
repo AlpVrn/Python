e4de7d46ad186d809c5ff6c5edbbda24c40df564
AlpVrn <mhmtalpvrn@gmail.com>
2022-11-23 16:02:14 +0300

    dosya cart curt

---
+++ PythondaHataveHataYonetimi/errors-demo.py
@@ -0,0 +1,38 @@
+import re
+liste = ["1","2","3","5a","10b","abc","10","50"]
+
+# 1: liste elemanları içindeki sayısal değerleri bulunuz
+for x in liste:
+    try:
+        print(int(x))
+    except(ValueError):
+        continue
+#2 : kullanıcı 'q' değerini girmedikçe aldığınız her input sayı
+#olduğundan emin olunuz aksi halde hata mesajı yazınız
+# while True:
+#     try:
+#         deger=int(input('Bir sayı giriniz'))
+#         if deger=="q":
+#             break
+#     except Exception as a:
+#         print('aaa tekrar dene')
+# print('Floop is over')
+
+# while True:
+#     sayi = input('sayi : ')
+#     if sayi =='q':
+#         break
+
+#     try:
+#         result = float(sayi)
+#         print('girdiğiniz sayi ' , result)
+#     except ValueError:
+#         print('geçersiz sayi')
+#         continue
+
+
+#3: girilen parola içinde türkçe karakter hatası veriniz
+password=input('Bir sifre giriniz turkce karakter kullanmayaniz')
+if(re.search("[ı,ğ,ü,ö,ç]",password)):
+    raise Exception("türkçe karakter kullanma")
+#4 faktoriyel fonksiyonu oluşturup fonksiyona geleen değer için hata veriniz
