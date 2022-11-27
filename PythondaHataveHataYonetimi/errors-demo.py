45f7e3de2997feead7be3a38aaa50e7b40e4adfc
AlpVrn <mhmtalpvrn@gmail.com>
2022-11-27 23:05:17 +0300

    aa

--- PythondaHataveHataYonetimi/errors-demo.py
+++ PythondaHataveHataYonetimi/errors-demo.py
@@ -1,12 +1,12 @@
 import re
 liste = ["1","2","3","5a","10b","abc","10","50"]
 
-# 1: liste elemanları içindeki sayısal değerleri bulunuz
-for x in liste:
-    try:
-        print(int(x))
-    except(ValueError):
-        continue
+# # 1: liste elemanları içindeki sayısal değerleri bulunuz
+# for x in liste:
+#     try:
+#         print(int(x))
+#     except(ValueError):
+#         continue
 #2 : kullanıcı 'q' değerini girmedikçe aldığınız her input sayı
 #olduğundan emin olunuz aksi halde hata mesajı yazınız
 # while True:
@@ -32,9 +32,25 @@ for x in liste:
 
 
 #3: girilen parola içinde türkçe karakter hatası veriniz
-password=input('Bir sifre giriniz turkce karakter kullanmayaniz')
-if(re.search("[ı,ğ,ü,ö,ç]",password)):
-    raise Exception("türkçe karakter kullanma")
+# password=input('Bir sifre giriniz turkce karakter kullanmayaniz')
+# if(re.search("[ı,ğ,ü,ö,ç]",password)):
+#     raise Exception("türkçe karakter kullanma")
+
+
 #4 faktoriyel fonksiyonu oluşturup fonksiyona 
 # geleen değer için hata veriniz
 
+
+def calculationFactorial ():
+  try:
+    number = int (input("Lütfen faktöriyelini bulmak istediğiniz sayıyı giriniz...\n"))
+    factorial = 1
+    
+    if number >= 0:
+        for i in range (1, number+1): #range varsayılan olarak 0'dan başlar ve son değeri kapsamaz
+        factorial = factorial*i
+        print("Girdiğiniz sayının faktöriyeli:",factorial)
+  except Exception as a:
+      print(f"hat",a)
+
+calculationFactorial ()
