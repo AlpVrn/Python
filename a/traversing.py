11a5576959d3d375ef3bc9b61f1897d8c8a38d26
AlpVrn <107107552+AlpVrn@users.noreply.github.com>
2022-11-29 14:54:13 +0300

    Delete a directory

--- a/traversing.py
+++
@@ -1,7 +0,0 @@
-with open("newfile.txt","r",encoding="utf-8") as file:
-    contenct = file.read(10)
-    print(contenct)
-    file.seek(0)
-    print(file.tell())
-    contenct2 = file.read()
-    print(contenct2)
